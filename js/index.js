cfdbb3d11063458cc3768368bc824c054346e0b5
karlisnu <karlacruzk09@gmail.com>
2026-05-18 11:14:17 -0700

    cambios

--- js/index.js
+++ js/index.js
@@ -1,9 +1,40 @@
 // Archivo: \js\index.js
 
 function tutores(){
-    window.location.href = '/Inicio/RegistroTutor.html';
+ 
+     window.location.href = '/Inicio/RegistroTutor.html';;
+
+   
 }
 
 function alumnos(){
-    window.location.href = '/Inicio/RegistroAlumnos.html';
+ 
+   window.location.href = '/Inicio/RegistroAlumnos.html';
+
+}
+
+function vinculaT(){
+
+    const usuario = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(usuario !== "" && password !== ""){
+        window.location.href = '/Inicio/RegistroTutor.html';
+    } else {
+        alert("Completa todos los campos");
+    }
+
+}
+
+function vinculaA(){
+
+    const usuario = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(usuario !== "" && password !== ""){
+        window.location.href = '/Inicio/RegistroAlumnos.html';
+    } else {
+        alert("Completa todos los campos");
+    }
+
 }
